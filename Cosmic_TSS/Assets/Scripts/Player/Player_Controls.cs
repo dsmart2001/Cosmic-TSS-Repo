f5e219f7b9cb998432f9b637cc2c4ec050f73f52
Dallas Smart <dsmart10@outlook.com>
2021-09-15 21:55:45 +1000

    Player: First iterations of standard movement

--- Cosmic_TSS/Assets/Scripts/Player/Player_Controls.cs
+++ Cosmic_TSS/Assets/Scripts/Player/Player_Controls.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class Player_Controls : MonoBehaviour
 {
+    /*
+     * 
+     */
+
+    // Player objects and components
+    private Rigidbody RB => GetComponent<Rigidbody>();
+
+    // Player stats
+    private Vector3 movementVector;
+    public float movementSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,11 @@ public class Player_Controls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movementVector = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        RB.MovePosition(RB.position + (movementVector * movementSpeed * Time.deltaTime));
     }
 }
