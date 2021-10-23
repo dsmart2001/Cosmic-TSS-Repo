ddc304be7fd180f81d9a4b1f80086b731be89b43
Dallas Smart <dsmart10@outlook.com>
2021-10-23 17:55:54 +1000

    Added explosive enemy

--- Cosmic_TSS/Assets/Explosive_Barrel.cs
+++ Cosmic_TSS/Assets/Explosive_Barrel.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class Explosive_Barrel : MonoBehaviour
 {
     public GameObject explosion;
+    public float integrity = 150;
 
     private void Start()
     {
         explosion.SetActive(false);
     }
+
     private void OnCollisionEnter(Collision c)
     {
         string tag = c.gameObject.tag;
 
         if (tag == "Bullet")
         {
-            explosion.SetActive(true);
-            StartCoroutine(Explode());
+            integrity -= c.gameObject.GetComponent<Weapon_BulletVelocity>().damage;
+
+            if(integrity <= 0)
+            {
+                explosion.SetActive(true);
+                StartCoroutine(Explode());
+            }
         }
     }
-
 
     public IEnumerator Explode()
     {
